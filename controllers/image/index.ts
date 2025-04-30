b29ff88d9b641f3b412d5a2e5f64a1c4787faf4f
Uthman Abdulganiyu <uthmanabdulganiyu2019@gmail.com>
2025-04-30 21:41:08 +0100

    [feat] initialize project with server, auth, and image generation functionality

---
+++ controllers/image/index.ts
@@ -0,0 +1,24 @@
+import { Request, Response } from "express";
+import { imageModel } from "../../models/image";
+import dotenv from "dotenv";
+dotenv.config();
+
+const url = process.env.API_URL;
+export const generateImage = async (req: Request, res: Response) => {
+  const { prompt } = req.body;
+  try {
+    const imageUrl = `${url}${encodeURIComponent(prompt)}`;
+
+    const image = await imageModel.create({ prompt, url: imageUrl });
+    res.status(201).json({ success: true, data: image });
+  } catch (error) {
+    res.json({ success: false, error });
+  }
+};
+
+export const getImages = async (req: Request, res: Response) => {
+  const images = await imageModel
+    .find({ userId: (req as any).user.id })
+    .sort({ createdAt: -1 });
+  res.json(images);
+};
---
+++ models/image.ts
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+
+const imageSchema = new Schema(
+  {
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: "user" },
+    prompt: { type: String },
+    imageUrl: { type: String },
+  },
+  { timestamps: true }
+);
+
+export const imageModel = mongoose.model("image", imageSchema);
