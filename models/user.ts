fc51094b08bf932c305a08a6e5424e3de6a47962
Auspicious <uthmanabdulganiyu2019@gmail.com>
2025-07-10 17:02:36 +0100

    Update user.model

--- models/user.ts
+++ models/user.ts
@@ -1,3 +1,32 @@
+/*import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema(
+  {
+    firstName: { type: String, required: true, min: 3 },
+    lastName: { type: String, required: true, min: 3 },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+    },
+    password: { type: String, required: true, min: 6 },
+    manageOTP: {
+      otp: { type: Number },
+      otpDate: { type: Number },
+    },
+  },
+  { timestamps: true }
+);
+
+
+
+export const userModel = mongoose.model("user", userSchema);
+*/
+
+
 import mongoose from "mongoose";
 
 const Schema = mongoose.Schema;
@@ -16,6 +45,11 @@ const userSchema = new Schema(
     manageOTP: {
       otp: { type: Number },
       otpDate: { type: Number },
+    },
+    generationCount: { type: Number, default: 0 }, 
+    lastResetDate: { 
+      type: Date, 
+      default: () => new Date(new Date().setUTCHours(0, 0, 0, 0)) 
     },
   },
   { timestamps: true }
